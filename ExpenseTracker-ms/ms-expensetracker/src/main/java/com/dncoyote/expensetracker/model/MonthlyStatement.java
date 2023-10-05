384ad93a194b18f775eb162102a963e7cee81c0a
BilalThaha <bilal.athaha@gmail.com>
2023-10-05 07:24:54 +0530

    first commit

---
+++ ExpenseTracker-ms/ms-expensetracker/src/main/java/com/dncoyote/expensetracker/model/MonthlyStatement.java
@@ -0,0 +1,22 @@
+package com.dncoyote.expensetracker.model;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MonthlyStatement {
+
+    private String category;
+    private String type;
+    private double amount;
+    private String description;
+    private Date date;
+
+}
